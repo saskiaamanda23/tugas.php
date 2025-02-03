8b7e598981c1152f0bee0026c8c6b0bc91748465
reysa27 <damayantireysa@gmail.com>
2025-02-03 21:00:55 +0700

    CRUD genre

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    GenreController,
+    CastController
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//route untuk menampilkan from genre
+Route::get('/genre/create',[GenreController::class, 'create'])->name('genre.create');
+Route::post('/genre', [GenreController::class,'store'])->name('genre.store');
+Route::get('/genre', [GenreController::class, 'index'])->name('genre.index');
+Route::get('/genre/{id}/edit', [GenreController::class, 'edit'])->name('genre.edit');
+Route::put('/genre/{id}',[GenreController::class, 'update'])->name('genre.update');
+Route::get('/genre/{id}', [GenreController::class, 'show'])->name('genre.show');
+Route::delete('/genre/{id}', [GenreController::class, 'destroy'])->name('genre.delete');
+
+//route casts
+
+Route::get('/cast',[CastController::class, 'index'])->name('cast.index');
+Route::get('/cast/create', [CastController::class, 'create'])->name('cast.create');
+Route::post('/cast', [CastController::class, 'store'])->name('cast.store');
+Route::get('/cast/{id}/edit', [CastController::class, 'edit'])->name('cast.edit');
+Route::put('/cast/{id}', [CastController::class, 'update'])->name('cast.update');
+Route::delete('/cast/{id}', [CastController::class, 'destroy'])->name('cast.delete');
